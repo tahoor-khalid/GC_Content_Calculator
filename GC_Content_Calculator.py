3b59eb698a1cac44633822bb1953bf1945d0841c
Tahoor Khalid <tahoor.haseeb8@gmail.com>
2026-07-22 11:05:36 +0500

    Upgraded GC_Content_Calculator to support FASTA files using Biopython

--- GC_Content_Calculator.py
+++ GC_Content_Calculator.py
@@ -1,31 +1,23 @@
-<<<<<<< HEAD
+from Bio import SeqIO
+record = SeqIO.read("sequence.fasta", "fasta")
+sequence = str(record.seq)
+print(record.id)
+print(record.description)
+print(record.seq)
+print(len(sequence))
+
+
 def percentage_GC(sequence): #Defining a function in python
-    bases = sequence
-    total_bases = len(bases) #Finding the length of the sequence
+    total_bases = len(sequence) #Finding the length of the sequence
     
-    GC_content = bases.count("G")+bases.count("C") #Finding the number of G's and C's in the sequence and adding them together
+    GC_content = sequence.count("G")+ sequence.count("C") #Finding the number of G's and C's in the sequence and adding them together
    
     percentage_GC = (GC_content/total_bases)*100 #Calculating the percentage of GC 
     
     GC_rounded = round(percentage_GC,2) #Rounding off the answer
     return(GC_rounded)
 
-sequence = "ATCTGTCGTACGTCAGT"
-print(sequence)
 print(percentage_GC(sequence), "%")
-=======
-def percentage_GC(sequence): #Defining a function in python
-    bases = sequence
-    total_bases = len(bases) #Finding the length of the sequence
-    
-    GC_content = bases.count("G")+bases.count("C") #Finding the number of G's and C's in the sequence and adding them together
-   
-    percentage_GC = (GC_content/total_bases)*100 #Calculating the percentage of GC 
-    
-    GC_rounded = round(percentage_GC,2) #Rounding off the answer
-    return(GC_rounded)
 
-sequence = "ATCTGTCGTACGTCAGT"
-print(sequence)
-print(percentage_GC(sequence), "%")
->>>>>>> ed591baef2aff6f64e5c6ba59b99d1c7e585a380
+
+
